5d25abeeaea93ec56df8405d7d7e0c7e51fa1686
Chance Neff and Ewa Manek <ewa@>
2016-09-14 13:27:51 -0700

    finish ui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,21 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/jobOpenings", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      City city = City.find(Integer.parseInt(request.queryParams("cityId")));
+
+      String title = request.queryParams("title");
+      String contactInfo = request.queryParams("contactInfo");
+      String description = request.queryParams("description");
+      JobOpening newJobOpening = new JobOpening(title, contactInfo, description);
+      city.addJobOpening(newJobOpening);
+      model.put("city", city);
+      model.put("template", "templates/city-jobOpenings-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/cities/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/city-form.vtl");
@@ -35,6 +50,23 @@ public class App {
       model.put("template", "templates/cities.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/cities/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("cities/:id/jobOpenings/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city-jobOpenings-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
